9cf2fed8e8aff01019ef9fbb14a6fe00bc8928eb
Lim Seong-Gook <sklim0932@gmail.com>
2021-03-27 03:20:33 +0900

    회원 domain, repository, service 구현

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() {
+
+    }
+}
